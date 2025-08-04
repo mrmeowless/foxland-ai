e69a7f544e9938da05707b0f00a5d451e641cfbb
mrmeowless <mrmeowless@ya.ru>
2025-08-04 15:01:05 +0300

    Fixed bugs

--- src/utils/tts.js
+++ src/utils/tts.js
@@ -1,18 +1,24 @@
 export async function speakTTS(text) {
-	const response = await fetch('https://foxland-ai.onrender.com/v1/tts', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({ text, voiceId: '8HSRAwEWAAa6wv9cdi5S' }),
-	})
+	const voiceId = '8HSRAwEWAAa6wv9cdi5S'
 
-	if (!response.ok) {
-		console.error('TTS Error')
-		return
+	try {
+		const response = await fetch('https://foxland-ai.onrender.com/v1/tts', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ text, voiceId }),
+		})
+
+		if (!response.ok) {
+			console.error('TTS Error')
+			return
+		}
+
+		const audioBlob = await response.blob()
+		const audioUrl = URL.createObjectURL(audioBlob)
+		new Audio(audioUrl).play()
+	} catch (error) {
+		console.error('Ошибка TTS: ', error)
 	}
-
-	const audioBlob = await response.blob()
-	const audioUrl = URL.createObjectURL(audioBlob)
-	new Audio(audioUrl).play()
 }
